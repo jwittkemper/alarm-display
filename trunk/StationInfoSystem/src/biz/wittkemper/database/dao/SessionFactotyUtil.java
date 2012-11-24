be6f08f2cedf0944df6d8e831709115412b30081
joerg@wittkemper.biz <joerg@wittkemper.biz@91f8f1f7-ae2d-07fb-77c3-d934ea6564e4>
2012-11-24 07:56:03 +0000

    Aktuelle Version.

--- trunk/StationInfoSystem/src/biz/wittkemper/database/dao/SessionFactotyUtil.java
+++ trunk/StationInfoSystem/src/biz/wittkemper/database/dao/SessionFactotyUtil.java
@@ -5,56 +5,66 @@ import java.util.Properties;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import biz.wittkemper.utils.Utils;
 import biz.wittkemper.utils.Utils.STATIONPROP;
 
 public class SessionFactotyUtil {
-
+	Logger logger = LoggerFactory.getLogger(SessionFactotyUtil.class);
 	private static SessionFactory sessionFactory;
+
 	/**
 	 * disable contructor to guaranty a single instance
 	 */
 	private SessionFactotyUtil() {
 	}
 
-	static{
+	static {
 		String db = Utils.getPropertie(STATIONPROP.DATABASE);
-		
+
 		Properties properties = new Properties();
-		properties.setProperty("hibernate.connection.url", "jdbc:mysql://"+ db.trim() + "/monitord");
+		properties.setProperty("hibernate.connection.url",
+				"jdbc:mysql://" + db.trim() + "/monitord");
 		sessionFactory = new AnnotationConfiguration()
-			.addProperties(properties)
-			.setProperty("hibernate.connection.username", "monitord")
-			.setProperty("hibernate.connection.password", "monitord")
-			
-			.addAnnotatedClass(biz.wittkemper.database.entity.Kennung.class)
-			.addAnnotatedClass(biz.wittkemper.database.entity.Posac_status.class)
-			.addAnnotatedClass(biz.wittkemper.database.entity.Monitord_POSAC.class)
-			.addAnnotatedClass(biz.wittkemper.database.entity.Alarmierung.class)
-			.addAnnotatedClass(biz.wittkemper.database.entity.KalenderTyp.class)
-			.addAnnotatedClass(biz.wittkemper.database.entity.Kalender.class)
-			.configure().buildSessionFactory();
-		
+				.addProperties(properties)
+				.setProperty("hibernate.connection.username", "monitord")
+				.setProperty("hibernate.connection.password", "monitord")
+
+				.addAnnotatedClass(biz.wittkemper.database.entity.Kennung.class)
+				.addAnnotatedClass(
+						biz.wittkemper.database.entity.Posac_status.class)
+				.addAnnotatedClass(
+						biz.wittkemper.database.entity.Monitord_POSAC.class)
+				.addAnnotatedClass(
+						biz.wittkemper.database.entity.Alarmierung.class)
+				.addAnnotatedClass(
+						biz.wittkemper.database.entity.KalenderTyp.class)
+				.addAnnotatedClass(
+						biz.wittkemper.database.entity.Kalender.class)
+				.configure().buildSessionFactory();
+
 	}
 
 	public static SessionFactory getInstance() {
 		return sessionFactory;
 	}
 
-  /**
-   * Opens a session and will not bind it to a session context
-   * @return the session
-   */
+	/**
+	 * Opens a session and will not bind it to a session context
+	 * 
+	 * @return the session
+	 */
 	public Session openSession() {
 		return sessionFactory.openSession();
 	}
 
 	/**
-   * Returns a session from the session context. If there is no session in the context it opens a session,
-   * stores it in the context and returns it.
-	 * This factory is intended to be used with a hibernate.cfg.xml
-	 * including the following property <property
+	 * Returns a session from the session context. If there is no session in the
+	 * context it opens a session, stores it in the context and returns it. This
+	 * factory is intended to be used with a hibernate.cfg.xml including the
+	 * following property <property
 	 * name="current_session_context_class">thread</property> This would return
 	 * the current open session or if this does not exist, will create a new
 	 * session
@@ -65,10 +75,10 @@ public class SessionFactotyUtil {
 		return sessionFactory.getCurrentSession();
 	}
 
-  /**
-   * closes the session factory
-   */
-	public static void close(){
+	/**
+	 * closes the session factory
+	 */
+	public static void close() {
 		if (sessionFactory != null)
 			sessionFactory.close();
 		sessionFactory = null;
--- trunk/StationInfoSystem/src/biz/wittkemper/gui/BlitzTimer.java
+++ trunk/StationInfoSystem/src/biz/wittkemper/gui/BlitzTimer.java
@@ -1,27 +1,35 @@
 package biz.wittkemper.gui;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.util.TimerTask;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class BlitzTimer extends TimerTask {
 
 	JPanel master;
 	JPanel links;
 	JPanel rechts;
+	JPanel mitte;
+
+	JLabel text;
+
 	Color color1;
 	Color color2;
 	private static boolean blitz;
 
-	public BlitzTimer(JPanel master, JPanel links, JPanel rechts) {
+	public BlitzTimer(JPanel master, JPanel links, JPanel rechts, JPanel mitte,
+			JLabel text) {
 		this.master = master;
 		this.links = links;
 		this.rechts = rechts;
+		this.mitte = mitte;
+		this.text = text;
+
 		color1 = Color.RED;
 		color2 = Color.YELLOW;
-		
+
 	}
 
 	@Override
@@ -30,14 +38,20 @@ public class BlitzTimer extends TimerTask {
 			master.setBackground(color1);
 			links.setBackground(color2);
 			rechts.setBackground(color2);
+			mitte.setBackground(color2);
+			text.setBackground(color1);
 		} else {
 			master.setBackground(color2);
 			links.setBackground(color1);
 			rechts.setBackground(color1);
+			mitte.setBackground(color1);
+			text.setBackground(color2);
 		}
 		master.repaint();
 		links.repaint();
 		rechts.repaint();
+		mitte.repaint();
+
 		blitz = !blitz;
 	}
 
